aca124b06d47795d3cd22ee5bb819c9a433bd9a0
Clinton Lunn <clunn@utah.gov>
2024-06-18 11:05:15 -0600

    remove uploadthing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
-import '@uploadthing/react/styles.css';
+// import '@uploadthing/react/styles.css';
 import type { Metadata } from 'next';
 import NextTopLoader from 'nextjs-toploader';
 import { Inter } from 'next/font/google';
@@ -21,9 +21,11 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
-        <NextTopLoader />
-        <Toaster />
-        {children}
+        <Providers>
+          <NextTopLoader />
+          <Toaster />
+          {children}
+        </Providers>
 
       </body>
     </html>
--- components/layout/providers.tsx
+++ components/layout/providers.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import ThemeProvider from './ThemeToggle/theme-provider';
 import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 export default function Providers({
-  session,
+  // session,
   children
 }: {
-  session: SessionProviderProps['session'];
+  // session: SessionProviderProps['session'];
   children: React.ReactNode;
 }) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        {/* <SessionProvider session={session}>{children}</SessionProvider> */}
+        {children}
       </ThemeProvider>
     </>
   );
